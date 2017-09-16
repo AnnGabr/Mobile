b5dcc94ee4c50d99bae242f4327dcb6ddd3fb92f
AnnGabr <annabaks1997@gmail.com>
2017-09-17 00:07:00 +0300

    views added on activity

--- lab_2_guess_number/src/main/java/anngabr/androidlabs/MainActivity.java
+++ lab_2_guess_number/src/main/java/anngabr/androidlabs/MainActivity.java
@@ -2,6 +2,7 @@ package anngabr.androidlabs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
