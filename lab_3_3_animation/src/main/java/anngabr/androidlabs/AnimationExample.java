dba6a1ed0dbe952857565ad0810d2cceae1eafd1
AnnGabr <annabaks1997@gmail.com>
2017-09-25 22:07:37 +0300

    animation examples project created

---
+++ lab_3_3_animation/src/main/java/anngabr/androidlabs/AnimationExample.java
@@ -0,0 +1,13 @@
+package anngabr.androidlabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnimationExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation_example);
+    }
+}
